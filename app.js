715a349201edc7b8f40f1338c124751857abc579
IrohOmolola <irohomolola@gmail.com>
2021-04-30 17:12:29 +0000

    register

---
+++ app.js
@@ -0,0 +1,9 @@
+let myFunction = () => {
+    var x = document.getElementById("msg");
+    x.className = "show";
+    setTimeout(function() {
+        x.className =
+            x.className.replace("show", "");
+    }, 3000);
+}
+myFunction();
